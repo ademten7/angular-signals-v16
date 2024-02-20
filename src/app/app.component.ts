6e5c97131cd7283ea5ec2b0b401b051d00bd6bdf
ademten7 <ademten7@gmail.com>
2024-02-21 00:11:45 +0100

    added ng-zorro-badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { VehicleListComponent } from "./components/vehicle-list/vehicle-list.component";
+import { CartService } from './services/cart.service';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, VehicleListComponent]
+    imports: [CommonModule, RouterOutlet, NzIconModule,NzBadgeModule, NzLayoutModule, NzMenuModule, VehicleListComponent]
 })
 export class AppComponent {
   isCollapsed = false;
+  cartService = inject(CartService)
+
+  cartCount = computed(()=> this.cartService.cartItems().reduce((acc,item)=> acc+item.quantity,0));
 }
